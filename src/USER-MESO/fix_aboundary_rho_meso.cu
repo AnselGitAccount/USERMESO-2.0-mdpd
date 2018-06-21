b9669fb0287de7c7c46f002d4cdd9a7aaea667a8
Ansel Blumers <ansel_blumers@brown.edu>
2018-06-21 09:08:26 -0600

    added computation for phi and aribtrary boundary

--- src/USER-MESO/fix_aboundary_rho_meso.cu
+++ src/USER-MESO/fix_aboundary_rho_meso.cu
@@ -31,28 +31,75 @@ FixArbitraryBoundaryRho::FixArbitraryBoundaryRho( LAMMPS *lmp, int narg, char **
     Fix( lmp, narg, arg ),
     MesoPointers( lmp )
 {
-    cut_rho       = 0.;
+    cut_rho     = 0.;
+    cut_phi		= 0.;
+    phi_c		= 0.;
+    rho_factor	= 0.;
+    phi_factor  = 0.;
+    dw_factor   = 0.;
+
 
     for(int i = 0 ; i < narg ; i++) {
         if (!strcmp(arg[i],"cut_rho")) {
             cut_rho = atof( arg[++i] );
             continue;
         }
+        if (!strcmp(arg[i],"cut_phi")) {
+        	cut_phi = atof( arg[++i] );
+        	continue;
+        }
+        if (!strcmp(arg[i],"phi_c")) {
+        	phi_c = atof( arg[++i] );
+        	continue;
+        }
+        if (!strcmp(arg[i],"rho_wall")) {
+        	rho_wall = atof( arg[++i] );
+        	continue;
+        }
+        if (!strcmp(arg[i],"wall")) {
+        	if ( (wall_group = group->find( arg[++i] ) ) == -1 )
+        		error->all( FLERR, "<MESO> Undefined wall group id in fix aboundary/rho/meso" );
+            wall_groupbit = group->bitmask[ wall_group ];
+            continue;
+        }
+        if (!strcmp(arg[i],"mobile")) {
+        	if ( (mobile_group = group->find( arg[++i] ) ) == -1 )
+        		error->all( FLERR, "<MESO> Undefined fluid group id in fix aboundary/rho/meso" );
+            mobile_groupbit = group->bitmask[ mobile_group ];
+        }
     }
 
-    r64 rc_3 = cut_rho*cut_rho*cut_rho;
-    r64 rc_7 = rc_3*rc_3*cut_rho;
+    /*  groupbit MUST BE all particles (fluid + wall).
+     *  mobile_groupbit is fluid.
+     *  wall_groupbit is wall.
+     */
+
+    if ( mobile_groupbit & wall_groupbit ) {
+        error->warning( FLERR, "<MESO> Fix aboundary/rho/meso mobile and wall group overlap", true );
+    }
+
+    r64 rc_7 = cut_rho*cut_rho*cut_rho*cut_rho*cut_rho*cut_rho*cut_rho;
     rho_factor = 105.0 / (16.0 * M_PI * rc_7);
 
+    rc_7 = cut_phi*cut_phi*cut_phi*cut_phi*cut_phi*cut_phi*cut_phi;
+    phi_factor = 105.0 / (16.0 * M_PI * rho_wall * rc_7);
+    dw_factor = -315.0 / ( 4.0 * M_PI * rho_wall * rc_7);
 
-    if( cut_rho==0 )
-    	error->all( FLERR, "Fix parameters are not set");
+
+    if( cut_rho==0 | cut_phi==0 | phi_c==0 | rho_factor==0 | phi_factor==0 | dw_factor==0 )
+    	error->all( FLERR, "<MESO> Fix aboundary/rho/meso parameters are not set properly.");
+}
+
+void FixArbitraryBoundaryRho::init() {
+	dtv = update->dt;
+	dtf = 0.5 * update->dt * force->ftm2v;
 }
 
 int FixArbitraryBoundaryRho::setmask()
 {
     int mask = 0;
     mask |= FixConst::PRE_FORCE;
+    mask |= FixConst::POST_INTEGRATE;
     return mask;
 }
 
@@ -68,39 +115,41 @@ __global__ void gpu_compute_rho(
     const r64 rho_factor
     )
 {
-    for( int i = blockIdx.x * blockDim.x + threadIdx.x; i < n; i += gridDim.x * blockDim.x ) {
-    	if( mask[i] & groupbit ) {
-    		f3u  coord1 = tex1Dfetch<float4>( tex_coord, i ); // must fetch from texture, because shifted by domain local center
-    		int  n_pair = pair_count[i];
-    		int *p_pair = pair_table + ( i - __laneid() ) * pair_padding + __laneid();
+	// for all particles (fluid + wall).
+	for( int i = blockIdx.x * blockDim.x + threadIdx.x; i < n; i += gridDim.x * blockDim.x ) {
+        if ( !(mask[i] & groupbit) ) continue;
 
-    		r64 rho_i = 0.;
-    		for( int p = 0; p < n_pair; p++ ) {
-    			int j   = __lds( p_pair );
-    			p_pair += pair_padding;
-    			if( ( p & (WARPSZ - 1) ) == WARPSZ - 1 ) p_pair -= WARPSZ * pair_padding - WARPSZ;
+		f3u  coord1 = tex1Dfetch<float4>( tex_coord, i ); // must fetch from texture, because shifted by domain local center
+		int  n_pair = pair_count[i];
+		int *p_pair = pair_table + ( i - __laneid() ) * pair_padding + __laneid();
 
-    			f3u coord2   = tex1Dfetch<float4>( tex_coord, j );
-    			r32 dx       = coord1.x - coord2.x;
-    			r32 dy       = coord1.y - coord2.y;
-    			r32 dz       = coord1.z - coord2.z;
-    			r32 rsq      = dx * dx + dy * dy + dz * dz;
-    			r32 rinv     = rsqrt( rsq );
-    			r32 r        = rsq * rinv;
-    			r32 h        = cut_rho;
+		r64 rho_i = 0.;
+		for( int p = 0; p < n_pair; p++ ) {
+			int j   = __lds( p_pair );
+			p_pair += pair_padding;
+			if( ( p & (WARPSZ - 1) ) == WARPSZ - 1 ) p_pair -= WARPSZ * pair_padding - WARPSZ;
+
+			f3u coord2   = tex1Dfetch<float4>( tex_coord, j );
+			r32 dx       = coord1.x - coord2.x;
+			r32 dy       = coord1.y - coord2.y;
+			r32 dz       = coord1.z - coord2.z;
+			r32 rsq      = dx * dx + dy * dy + dz * dz;
+			r32 rinv     = rsqrt( rsq );
+			r32 h        = cut_rho;
 
 
-    			// Calculate Many Body Density rho; Lucy Kernel;
-    			if ( rsq < h*h && rsq >= EPSILON_SQ ) {
-    				r32 wf = (h + 3.0 * r) * (h - r) * (h - r) * (h - r) * rho_factor;
-    				rho_i += tex1Dfetch<r64>( tex_mass, i ) * wf;
-    			}
-    		}
+			// Calculate Many Body Density rho; Lucy Kernel;
+			if ( rsq < h*h && rsq >= EPSILON_SQ ) {
+				r32 r  = rsq * rinv;
+				r32 wf = (h + 3.0 * r) * (h - r) * (h - r) * (h - r) * rho_factor;
+				rho_i += tex1Dfetch<r64>( tex_mass, i ) * wf;
+			}
+		}
 
-    		rho[i] = rho_i;
-//			printf("%d %09.7g \n", i, rho[i] );
-    	}
-    }
+		rho[i] = rho_i;
+		//			printf("%d %09.7g \n", i, rho[i] );
+
+	}
 }
 
 void FixArbitraryBoundaryRho::pre_force( __attribute__( ( unused ) ) int vflag )
@@ -156,9 +205,119 @@ void FixArbitraryBoundaryRho::setup_pre_force( int vflag )
 }
 
 
+__global__ void gpu_aboundary(
+    texobj tex_coord, texobj tex_mass,
+    int* __restrict mask,
+    r64* __restrict coord_x,   r64* __restrict coord_y,   r64* __restrict coord_z,
+    r64* __restrict veloc_x,   r64* __restrict veloc_y,   r64* __restrict veloc_z,
+    r64* __restrict phi,
+    int* __restrict pair_count, int* __restrict pair_table,
+    const int pair_padding,
+    const int mobile_bit,
+    const int wall_bit,
+    const int n,
+    const r64 cut_phi,
+    const r64 phi_factor,
+    const r64 dw_factor,
+    const r64 phi_c,
+    const r64 dtf,
+    const r64 dtv )
+{
+    for( int i = blockIdx.x * blockDim.x + threadIdx.x; i < n; i += gridDim.x * blockDim.x ) {
+        if ( !(mask[i] & mobile_bit) ) continue;  // if not fluid, skip the rest.
+
+        const float3 coord1 = make_float3( coord_x[i], coord_y[i], coord_z[i] );
+        const float3 v      = make_float3( veloc_x[i], veloc_y[i], veloc_z[i] );
+
+        int n_pair       = pair_count[i];
+        int *p_pair      = pair_table + ( i - __laneid() ) * pair_padding + __laneid();
+        r64 phi_i        = 0.f;
+        float3 nw        = make_float3( 0.f, 0.f, 0.f );
+
+        for ( int p = 0; p < n_pair; p++ ) {
+            int j   = __lds( p_pair );
+            p_pair += pair_padding;
+            if( ( p & (WARPSZ - 1) ) == WARPSZ - 1 ) p_pair -= WARPSZ * pair_padding - WARPSZ;
+
+            f3u coord2   = tex1Dfetch<float4>( tex_coord, j );
+            r64 dx       = coord1.x - coord2.x;
+            r64 dy       = coord1.y - coord2.y;
+            r64 dz       = coord1.z - coord2.z;
+            r64 rsq      = dx * dx + dy * dy + dz * dz;
+
+
+//            printf("%d %d %d\n",(mask[j] & wall_bit) ,(r < cut_phi), (mask[j] & wall_bit) && (r < cut_phi));
+
+            // If j is a wall particle.
+            if ( (mask[j] & wall_bit) && (rsq < cut_phi*cut_phi) )  {
+    			// compute boundary volume fraction phi & wall-normal.
+                r64 rinv     = rsqrt( rsq );
+                r64 r        = MAX(rsq * rinv, EPSILON);
+                r64 d        = cut_phi - r;
+    			phi_i       += (cut_phi + 3.*r) * d * d * d * phi_factor;
+    			nw           = nw - (r * d * d * dw_factor) * make_float3( dx, dy, dz ) * rinv;
+//    			printf("d %g phi_i %g\n",d, phi_i);
+            }
+        }
+        phi[i] = phi_i;  // only for fluid particle.
+
+        // if the particle across wall-plane: bounce-back
+        if ( phi_i > phi_c ) {
+            nw = normalize( nw );  // Now, nw is the unit vector.
+
+            // calculate the position prior to Initial_Integrate.
+            const r32 dtfm      = dtf / tex1Dfetch<r64>(tex_mass, i);
+            const float3 orig_x = coord1 - v * dtfm;
+            const r32 dot_prod  = dot( v, nw );
+            const r32 tmp     	= MAX( 0.f, dot_prod );  // remove the normal components
+
+            // Update based on bounce-back condition.
+            veloc_x[i]  = -v.x + 2.f * tmp * nw.x;
+            veloc_y[i]  = -v.y + 2.f * tmp * nw.y;
+            veloc_z[i]  = -v.z + 2.f * tmp * nw.z;
+            coord_x[i] += dtv * veloc_x[i];
+            coord_y[i] += dtv * veloc_y[i];
+            coord_z[i] += dtv * veloc_z[i];
+        }
+    }
+}
+
+
+void FixArbitraryBoundaryRho::post_integrate() {
+
+	MesoNeighList *dlist = meso_neighbor->lists_device[ force->pair->list->index ];
+	if( !dlist ) error->all( FLERR, "<MESO> fix aboundary/rho/meso must be used together with a pair from USER-MESO" );
+
+	meso_atom->meso_avec->dp2sp_merged( 0, 0, atom->nlocal+atom->nghost, true );
+
+	static GridConfig grid_cfg;
+	if (!grid_cfg.x) grid_cfg = meso_device->configure_kernel( gpu_aboundary );
+
+	gpu_aboundary <<< grid_cfg.x, grid_cfg.y, 0, meso_device->stream() >>> (
+			meso_atom->tex_coord_merged, meso_atom->tex_misc("mass"),
+			meso_atom->dev_mask,
+	        meso_atom->dev_coord(0), meso_atom->dev_coord(1), meso_atom->dev_coord(2),
+	        meso_atom->dev_veloc(0), meso_atom->dev_veloc(1), meso_atom->dev_veloc(2),
+            meso_atom->dev_phi,
+	        dlist->dev_pair_count_core, dlist->dev_pair_table,
+	        dlist->n_col,
+	        mobile_groupbit,
+	        wall_groupbit,
+	        atom->nlocal,
+	        cut_phi,
+	        phi_factor,
+	        dw_factor,
+	        phi_c,
+	        dtf,
+	        dtv
+	);
+}
+
+
 int FixArbitraryBoundaryRho::pack_comm( int n, int *list, double *buf, int pbc_flag, int *pbc )
 {
-    int m = 0;
+	// No phi, because only rho is updated in pre_force().
+	int m = 0;
     for( int i = 0; i < n; i++ ) {
         buf[m++] = atom->rho[ list[i] ];
     }
--- src/USER-MESO/fix_aboundary_rho_meso.h
+++ src/USER-MESO/fix_aboundary_rho_meso.h
@@ -25,17 +25,22 @@ class FixArbitraryBoundaryRho : public Fix, protected MesoPointers
 public:
     FixArbitraryBoundaryRho(LAMMPS *lmp, int narg, char **arg);
 
+    virtual void init();
     virtual int setmask();
     virtual void pre_force(int);
     virtual void setup_pre_force(int);
+    virtual void post_integrate();
     virtual int pack_comm(int, int *, double *, int, int *);
     virtual void unpack_comm(int, int, double *);
 
 
 protected:
     int wall_group, wall_groupbit;
+    int mobile_group, mobile_groupbit;
     r64 rc, sigma, frac, xcut;
-    r64 cut_rho, rho_factor;
+    r64 cut_rho, rho_factor, rho_wall;
+    r64 cut_phi, phi_factor, phi_c;
+    r64 dw_factor, dtv, dtf;
 };
 
 }
